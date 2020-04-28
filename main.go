2b96160330f1696b3fc38474b47eeb4f5f3167db
JaimeBR-ui <bambas@knights.ucf.edu>
2020-04-28 04:12:06 -0400

    GoLang's Hello World!

---
+++ main.go
@@ -0,0 +1,18 @@
+// Jaime Bohorquez
+// Programmed using Atom + Terminal on Ubuntu
+// This was derived from the GoLang course at codecademy.com
+
+// Make it part of package "main".
+package main
+
+// Import GoLang's io.
+import "fmt"
+
+// Main function.
+func main() {
+    // IO print string.
+    fmt.Println("Hello, world!");
+}
+
+// Notes:
+// Go doesn't exactly have classes, Go is a multi-paradigm prog lang.
