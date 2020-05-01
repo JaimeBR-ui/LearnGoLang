9be7bfceaaf1a6951c25295c08d77846110882ba
JaimeBR-ui <bambas@knights.ucf.edu>
2020-05-01 02:45:46 -0400

    Merge

--- random.go
+++ random.go
@@ -5,11 +5,7 @@ import (
     "math/rand"
 )
 
-<<<<<<< HEAD
 func genRandDie() int8 { // this defines the return type
-=======
-func genRandDie()  {
->>>>>>> bc86b1679638b5b488fb8e1f6f10b4253e76655d
     // Set the seed so we get a unique random value every time
     rand.Seed(time.Now().UnixNano())
     return rand.Intn(5) + 1 // (0 - 5) + 1
